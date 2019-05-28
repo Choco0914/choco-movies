bb7390edc82ed6e272fe40f1c60a5bcfb807ec30
Choco0914 <heojeongho1992@gmail.com>
2019-05-28 23:19:51 +0900

    Home Container part One

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,10 +1,28 @@
 import React from "react";
 import MoviesPresenter from "./MoviesPresenter";
+import { movies } from "../../api";
 
 export default class extends React.Component {
   state = {
-    loading: true
+    loading: true,
+    loading: true,
+    upcoming: null,
+    popular: null,
+    nowPlaying: null,
+    error: null
   };
+
+  async componentDidMount() {
+    try {
+      const upcoming = await movies.upcoming();
+      const popular = await movies.popular();
+      const nowPlaying = await movies.nowPlaying();
+    } catch {
+      this.setState({ error: "영화들을 가져올수 없습니다." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { loading } = this.state;
