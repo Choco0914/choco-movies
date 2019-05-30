209caa8123996090dcd5d4a8f5c42561e65ca522
Choco0914 <heojeongho1992@gmail.com>
2019-05-30 12:36:18 +0900

    Search Screen Part Two

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,29 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { BG_COLOR } from "../../constants/Colors";
+import { BG_COLOR, GREY_COLOR } from "../../constants/Colors";
+import Layouts from "../../constants/Layouts";
 
 const Container = styled.View`
   flex: 1;
   background-color: ${BG_COLOR};
 `;
 
-const Input = styled.TextInput``;
+const InputContainer = styled.View`
+  align-items: center;
+  margin-vertical: 20px;
+`;
+
+const Input = styled.TextInput`
+  background-color: rgba(255, 255, 255, 1);
+  width: ${Layouts.width / 1.6};
+  border-radius: 20px;
+  padding: 10px;
+  text-align: center;
+`;
 
 const SearchPresenter = ({
   loading,
   movieResults,
   tvResults,
   searchTerm,
-  handleSearchUpdate
+  handleSearchUpdate,
+  onSubmitEditing
 }) => (
   <Container>
-    <Input
-      onChangeText={handleSearchUpdate}
-      value={searchTerm}
-      autoFocus
-      returnKeyType={"go"}
-    />
+    <InputContainer>
+      <Input
+        onChangeText={handleSearchUpdate}
+        value={searchTerm}
+        returnKeyType="search"
+        placeholder="영화,TV를 검색해보세요"
+        placeholderTextColor={GREY_COLOR}
+        onSubmitEditing={onSubmitEditing}
+      />
+    </InputContainer>
   </Container>
 );
 
@@ -32,7 +49,8 @@ SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
   tvResults: PropTypes.array,
   searchTerm: PropTypes.string,
-  handleSearchUpdate: PropTypes.func.isRequired
+  handleSearchUpdate: PropTypes.func.isRequired,
+  onSubmitEditing: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
